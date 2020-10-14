38a7f331d3947b7f7c62b44f20283537494a90dd
habib zulfiqar <habibzulfiqar2017@gmail.com>
2020-10-14 18:44:58 +0500

    ignored node modules

--- templates/taxonomy.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Taxonomy</title>
-</head>
-<body>
-<h1>ny Taxonomy</h1>
-</body>
-</html>
--- templates/widget.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head>
-	<title>widger</title>
-</head>
-<body>
-<h1>My widget</h1>
-</body>
-</html>
